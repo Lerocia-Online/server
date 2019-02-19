c7bab012ebd446e584ceb295bf4ebb2b432c0283
Jeffrey Thor <JeffreyThor@users.noreply.github.com>
2019-02-19 10:05:36 -0500

    Adds NPCs (#15)

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -26,6 +26,15 @@ public class InventoryItem {
   public int itemId;
 }
 
+public class NPC {
+  public int npcId;
+  public string npcName;
+  public Vector3 position;
+  public Quaternion rotation;
+  public float moveTime;
+  public List<InventoryItem> inventory;
+}
+
 [Serializable]
 class DatabaseWorldItem {
   public int world_id;
@@ -38,6 +47,18 @@ class DatabaseWorldItem {
 [Serializable]
 public class DatabaseItem {
   public int item_id;
+}
+
+[Serializable]
+class DatabaseNPC {
+  public int npc_id;
+  public string npc_name;
+  public float position_x;
+  public float position_y;
+  public float position_z;
+  public float rotation_x;
+  public float rotation_y;
+  public float rotation_z;
 }
 
 public class Server : MonoBehaviour {
@@ -57,12 +78,15 @@ public class Server : MonoBehaviour {
 
   private float lastMovementUpdate;
   private float movementUpdateRate = 0.05f;
-  
+
   private List<WorldItem> worldItems = new List<WorldItem>();
+  private List<NPC> _npcs = new List<NPC>();
 
   private WWWForm form;
   private string getWorldItemsEndpoint = "get_world_items.php";
+  private string getNPCsEndpoint = "get_npcs.php";
   private string getItemsForUserEndpoint = "get_items_for_user.php";
+  private string getItemsForNPCEndpoint = "get_items_for_npc.php";
   private string addItemForUserEndpoint = "add_item_for_user.php";
   private string deleteItemForUserEndpoint = "delete_item_for_user.php";
   private string addWorldItemEndpoint = "add_world_item.php";
@@ -87,6 +111,25 @@ public class Server : MonoBehaviour {
 
     isStarted = true;
     StartCoroutine("GetWorldItems");
+    StartCoroutine("GetNPCs");
+  }
+
+  private IEnumerator GetNPCs() {
+    form = new WWWForm();
+
+    WWW w = new WWW(NetworkSettings.API + getNPCsEndpoint, form);
+    yield return w;
+
+    if (string.IsNullOrEmpty(w.error)) {
+      string jsonString = JsonHelper.fixJson(w.text);
+      DatabaseNPC[] dbnpc = JsonHelper.FromJson<DatabaseNPC>(jsonString);
+      foreach (DatabaseNPC npc in dbnpc) {
+        AddNPC(npc.npc_id, npc.npc_name, npc.position_x, npc.position_y, npc.position_z, npc.rotation_x, npc.rotation_y,
+          npc.rotation_z);
+      }
+    } else {
+      Debug.Log(w.error);
+    }
   }
 
   private IEnumerator GetWorldItems() {
@@ -105,7 +148,7 @@ public class Server : MonoBehaviour {
       Debug.Log(w.error);
     }
   }
-  
+
   private IEnumerator GetItemsForUser(int[] ids) {
     form = new WWWForm();
     int userId = ids[0];
@@ -125,19 +168,42 @@ public class Server : MonoBehaviour {
         item.itemId = it.item_id;
         clients.Find(x => x.connectionId == cnnId).inventory.Add(item);
       }
+
       string inventoryMessage = "INVENTORY|";
       foreach (InventoryItem item in clients.Find(x => x.connectionId == cnnId).inventory) {
         inventoryMessage += item.itemId + "|";
       }
+
       inventoryMessage = inventoryMessage.Trim('|');
-    
-      // ITEMS|0%3%10.12%42.11%4.82|1%2%10.12%42.11%4.82|2%3%10.12%42.11%4.82
+
       Send(inventoryMessage, reliableChannel, cnnId);
     } else {
       Debug.Log(w.error);
     }
   }
-  
+
+  private IEnumerator GetItemsForNPC(int npcId) {
+    form = new WWWForm();
+
+    form.AddField("npc_id", npcId);
+
+    WWW w = new WWW(NetworkSettings.API + getItemsForNPCEndpoint, form);
+    yield return w;
+
+    if (string.IsNullOrEmpty(w.error)) {
+      string jsonString = JsonHelper.fixJson(w.text);
+      DatabaseItem[] dbi = JsonHelper.FromJson<DatabaseItem>(jsonString);
+      _npcs.Find(x => x.npcId == npcId).inventory = new List<InventoryItem>();
+      foreach (DatabaseItem it in dbi) {
+        InventoryItem item = new InventoryItem();
+        item.itemId = it.item_id;
+        _npcs.Find(x => x.npcId == npcId).inventory.Add(item);
+      }
+    } else {
+      Debug.Log(w.error);
+    }
+  }
+
   private IEnumerator AddItemForUser(int[] args) {
     form = new WWWForm();
     int userId = args[0];
@@ -155,7 +221,7 @@ public class Server : MonoBehaviour {
       Debug.Log(w.error);
     }
   }
-  
+
   private IEnumerator DeleteItemForUser(int[] args) {
     form = new WWWForm();
     int userId = args[0];
@@ -173,11 +239,11 @@ public class Server : MonoBehaviour {
       Debug.Log(w.error);
     }
   }
-  
+
   private IEnumerator AddWorldItemCoroutine(float[] args) {
     form = new WWWForm();
-    int worldId = (int)args[0];
-    int itemId = (int)args[1];
+    int worldId = (int) args[0];
+    int itemId = (int) args[1];
     float x = args[2];
     float y = args[3];
     float z = args[4];
@@ -197,7 +263,7 @@ public class Server : MonoBehaviour {
       Debug.Log(w.error);
     }
   }
-  
+
   private IEnumerator DeleteWorldItemCoroutine(int worldId) {
     form = new WWWForm();
     form.AddField("world_id", worldId);
@@ -211,7 +277,7 @@ public class Server : MonoBehaviour {
       Debug.Log(w.error);
     }
   }
-  
+
   private IEnumerator Logout(int userId) {
     form = new WWWForm();
     form.AddField("user_id", userId);
@@ -225,7 +291,7 @@ public class Server : MonoBehaviour {
       Debug.Log(w.error);
     }
   }
-  
+
   private IEnumerator LogoutAllUsers() {
     form = new WWWForm();
 
@@ -271,23 +337,27 @@ public class Server : MonoBehaviour {
               float.Parse(splitData[4]), float.Parse(splitData[5]), float.Parse(splitData[6]),
               float.Parse(splitData[7]), float.Parse(splitData[8]));
             break;
-          case "CHARGE":
-            OnCharge(connectionId);
-            break;
           case "ATK":
             OnAttack(connectionId);
             break;
           case "HIT":
             OnHit(connectionId, int.Parse(splitData[1]), int.Parse(splitData[2]));
             break;
+          case "HITNPC":
+            OnHitNPC(connectionId, int.Parse(splitData[1]), int.Parse(splitData[2]));
+            break;
           case "USE":
             OnUse(connectionId, int.Parse(splitData[1]));
             break;
           case "DROP":
-            OnDrop(connectionId, int.Parse(splitData[1]), float.Parse(splitData[2]), float.Parse(splitData[3]), float.Parse(splitData[4]));
+            OnDrop(connectionId, int.Parse(splitData[1]), float.Parse(splitData[2]), float.Parse(splitData[3]),
+              float.Parse(splitData[4]));
             break;
           case "PICKUP":
             OnPickup(connectionId, int.Parse(splitData[1]));
+            break;
+          case "NPCITEMS":
+            OnNPCItems(connectionId, int.Parse(splitData[1]));
             break;
           default:
             Debug.Log("Invalid message : " + msg);
@@ -329,7 +399,7 @@ public class Server : MonoBehaviour {
     foreach (ServerClient sc in clients) {
       msg += sc.playerName + "%" + sc.connectionId + "|";
     }
-    
+
     msg = msg.Trim('|');
 
     // ASKNAME|3|DAVE%1|MICHAEL%2|TEMP%3
@@ -338,14 +408,28 @@ public class Server : MonoBehaviour {
     // Send all items placed in the world
     string itemsMessage = "ITEMS|";
     foreach (WorldItem item in worldItems) {
-      itemsMessage += item.world_id + "%" + item.itemId + "%" + item.position.x + "%" + item.position.y +
-                      "%" + item.position.z + "|";
+      itemsMessage += item.world_id + "%" + item.itemId + "%" + item.position.x + "%" + item.position.y + "%" +
+                      item.position.z + "|";
     }
 
     itemsMessage = itemsMessage.Trim('|');
-    
+
     // ITEMS|0%3%10.12%42.11%4.82|1%2%10.12%42.11%4.82|2%3%10.12%42.11%4.82
     Send(itemsMessage, reliableChannel, cnnId);
+
+    // Send all NPCs in the world
+    string npcsMessage = "NPCS|";
+    foreach (NPC npc in _npcs) {
+      npcsMessage += npc.npcId + "%" + npc.npcName + "%" + npc.position.x + "%" + npc.position.y + "%" +
+                     npc.position.z + "%" + npc.rotation.eulerAngles.x + "%" + npc.rotation.eulerAngles.y + "%" +
+                     npc.rotation.eulerAngles.z + "|";
+    }
+
+    npcsMessage = npcsMessage.Trim('|');
+
+    Debug.Log("Sending " + npcsMessage);
+    // NPCS|0%Harold|1%Johnny|2%Michelle
+    Send(npcsMessage, reliableChannel, cnnId);
   }
 
   private void OnDisconnection(int cnnId) {
@@ -376,28 +460,30 @@ public class Server : MonoBehaviour {
     clients.Find(c => c.connectionId == cnnId).moveTime = time;
   }
 
-  private void OnCharge(int cnnId) {
-    string msg = "CHARGE|" + cnnId;
-    Send(msg, reliableChannel, clients);
-  }
-  
-  private void OnAttack(int cnnId) {	
+  private void OnAttack(int cnnId) {
     string msg = "ATK|" + cnnId;
     Send(msg, reliableChannel, clients);
   }
-  
-  private void OnHit(int cnnId, int hitId, int damage) {	
+
+  private void OnHit(int cnnId, int hitId, int damage) {
+    //TODO Remove health from client on server
     string msg = "HIT|" + cnnId + "|" + hitId + "|" + damage;
     Send(msg, reliableChannel, clients);
   }
-  
+
+  private void OnHitNPC(int cnnId, int hitId, int damage) {
+    //TODO Remove health from NPC on server
+    string msg = "HITNPC|" + cnnId + "|" + hitId + "|" + damage;
+    Send(msg, reliableChannel, clients);
+  }
+
   private void OnUse(int cnnId, int itemId) {
     int[] args = {cnnId, itemId};
     StartCoroutine("DeleteItemForUser", args);
     string msg = "USE|" + cnnId + "|" + itemId;
     Send(msg, reliableChannel, clients);
   }
-  
+
   private void OnDrop(int cnnId, int itemId, float x, float y, float z) {
     int[] args = {clients.Find(c => c.connectionId == cnnId).userId, itemId};
     StartCoroutine("DeleteItemForUser", args);
@@ -407,6 +493,7 @@ public class Server : MonoBehaviour {
     } else {
       worldId = 0;
     }
+
     AddWorldItem(worldId, itemId, x, y, z);
     string msg = "DROP|" + cnnId + "|" + worldId + "|" + itemId + "|" + x + "|" + y + "|" + z;
     Send(msg, reliableChannel, clients);
@@ -414,13 +501,24 @@ public class Server : MonoBehaviour {
 
   private void OnPickup(int cnnId, int worldId) {
     InventoryItem item = new InventoryItem();
-    int[] args = {clients.Find(c => c.connectionId == cnnId).userId, worldItems.Find(i => i.world_id == worldId).itemId};
+    int[] args =
+      {clients.Find(c => c.connectionId == cnnId).userId, worldItems.Find(i => i.world_id == worldId).itemId};
     StartCoroutine("AddItemForUser", args);
     item.itemId = worldItems.Find(i => i.world_id == worldId).itemId;
     clients.Find(c => c.connectionId == cnnId).inventory.Add(item);
     DeleteWorldItem(worldId);
     string msg = "PICKUP|" + cnnId + "|" + worldId;
     Send(msg, reliableChannel, clients);
+  }
+
+  private void OnNPCItems(int cnnId, int npcId) {
+    string message = "NPCITEMS|" + npcId + "|";
+    foreach (InventoryItem item in _npcs.Find(c => c.npcId == npcId).inventory) {
+      message += item.itemId + "|";
+    }
+
+    message = message.Trim('|');
+    Send(message, reliableChannel, cnnId);
   }
 
   private void AddWorldItem(int worldId, int itemId, float x, float y, float z, bool onStart = false) {
@@ -438,6 +536,16 @@ public class Server : MonoBehaviour {
   private void DeleteWorldItem(int worldId) {
     worldItems.Remove(worldItems.Find(i => i.world_id == worldId));
     StartCoroutine("DeleteWorldItemCoroutine", worldId);
+  }
+
+  private void AddNPC(int npcId, string npcName, float px, float py, float pz, float rx, float ry, float rz) {
+    NPC npc = new NPC();
+    npc.npcId = npcId;
+    npc.npcName = npcName;
+    npc.position = new Vector3(px, py, pz);
+    npc.rotation = Quaternion.Euler(new Vector3(rx, ry, rz));
+    _npcs.Add(npc);
+    StartCoroutine("GetItemsForNPC", npcId);
   }
 
   private void Send(string message, int channelId, int cnnId) {
